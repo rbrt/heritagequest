dbc7fde0889b8b71f82a1d67dedd9de349c32d1e
Julia Tufts <juliatufts@gmail.com>
2013-10-18 21:55:24 -0300

    made a maze

---
+++ heritage_quest/Assets/movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		pos.z = gameObject.transform.position.z;
+		gameObject.transform.position = pos;
+		Debug.Log (Camera.main.ScreenToWorldPoint(pos));
+	}
+}
