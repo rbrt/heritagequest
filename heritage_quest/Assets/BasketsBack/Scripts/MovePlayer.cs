d6bf99ec448d58c374627f7b169b546a882aaab8
Rob <robbutler902@gmail.com>
2013-10-18 23:18:30 -0300

    movement script works

---
+++ heritage_quest/Assets/BasketsBack/Scripts/MovePlayer.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+	
+	bool isMovingLeft = false,
+		 isMovingRight = false,
+		 isJumping = false;
+	
+	float jumpVal = 10,
+		  moveSpeed = 20,
+	 	  jumpSpeed = 10;
+		
+	void Update () {
+		// Left
+		if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
+			isMovingLeft = true;
+			isMovingRight = false;
+		}
+		// Right
+		else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
+			isMovingRight = true;
+			isMovingLeft = false;
+		}
+		// Up - (climbing i guess?)
+		else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)){
+			
+		}
+		// Down (climbing i guess?)
+		else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)){
+			
+		}
+		// Jump
+		else if (Input.GetKeyDown(KeyCode.Space)){
+			isJumping = true;
+			StartCoroutine(JumpCoroutine());
+		}
+		
+		// Left
+		if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow)){
+			isMovingLeft = false;		
+		}
+		// Right
+		else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow)){
+			isMovingRight = false;
+		}
+		// Up - (climbing i guess?)
+		else if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow)){
+			
+		}
+		// Down (climbing i guess?)
+		else if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow)){
+			
+		}
+		
+		Vector3 pos = transform.position;
+		if (isMovingLeft){
+			pos.x = Mathf.Lerp(transform.position.x, transform.position.x - 1, Time.deltaTime * moveSpeed);
+		}
+		else if (isMovingRight){
+			pos.x = Mathf.Lerp(transform.position.x, transform.position.x + 1, Time.deltaTime * moveSpeed);
+		}
+		transform.position = pos;
+		
+	}
+	
+	IEnumerator JumpCoroutine(){
+		bool isFalling = false;
+		float jumpHeight = transform.position.y + jumpVal;
+		float baseHeight = transform.position.y;
+		while (isJumping){
+			if (transform.position.y >= jumpHeight -2){
+				isFalling = true;
+				isJumping = false;
+			}
+			else{
+				float height = Mathf.Lerp(transform.position.y, jumpHeight, Time.deltaTime * jumpSpeed);
+				Vector3 pos = transform.position;
+				pos.y = height;
+				transform.position = pos;
+				yield return null;
+			}
+		}
+		while (isFalling){
+			if (transform.position.y <= baseHeight + 2){
+				isFalling = false;
+				Vector3 pos = transform.position;
+				pos.y = baseHeight;
+				transform.position = pos;
+			}
+			else{
+				float height = Mathf.Lerp(transform.position.y, baseHeight, Time.deltaTime * jumpSpeed);
+				Vector3 pos = transform.position;
+				pos.y = height;
+				transform.position = pos;
+				yield return null;
+			}
+		}
+	}
+}
---
+++ heritage_quest/Assets/BasketsBack/Scripts/Scrollin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scrollin : MonoBehaviour {
+	
+	public GameObject player;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
